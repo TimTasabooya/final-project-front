b21123e8a4f1ae3097da3944b83bb346778e32bf
timtasabia@agentology.com <timtasabia@gmail.com>
2019-10-30 02:06:42 -0700

    side nav for favorites added to charts page

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 
-
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
